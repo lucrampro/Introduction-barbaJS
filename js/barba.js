e4d92eac4aae744486f5f5db5e7265606223ffc4
lucrampro <lucienramassamy@gmail.com>
2018-07-12 10:24:44 +0200

    premier commit

---
+++ js/barba.js
@@ -0,0 +1,16 @@
+Barba.Pjax.start();
+
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
+
+Barba.Pjax.getTransition = function() {
+  return HideShowTransition;
+};
