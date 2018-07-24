86e658c4c9e2ff965c9e139e308ad95582dc6e84
lucrampro <lucienramassamy@gmail.com>
2018-07-24 11:31:42 +0200

    toujour un echec

--- js/app.js
+++ js/app.js
@@ -1,37 +1,14 @@
-$('document').ready(function () {
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
 
-    var transEffect = Barba.BaseTransition.extend({
-
-        start: function () {
-
-            this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
-        },
-
-        fadeInNewcontent: function (nc) {
-
-            nc.hide();
-            var _this = this;
-
-            $(this.oldContainer).fadeOut(1000).promise().done(() => {
-
-                nc.css('visibility', 'visible');
-                nc.fadeIn(1000, function(){
-
-                    _this.done();
-
-
-                })
-
-            });
-        }
-    });
-
-    Barba.Pjax.getTransition = function(){
-
-        return transEffect;
-    }
-
-    Barba.Pjax.start();
-
-
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
 });
+
+Barba.Pjax.getTransition = function() {
+  return HideShowTransition;
+};
